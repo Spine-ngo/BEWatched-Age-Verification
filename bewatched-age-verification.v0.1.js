7694b31082e0a6cf83fcf1b6183e332d0a528937
Verdyck, Bram <bram.verdyck@wunderman.com>
2021-03-23 14:45:42 +0100

    initial commit

---
+++ bewatched-age-verification.v0.1.js
@@ -0,0 +1,326 @@
+window.BWAV = (function(window, BWAV_SETTINGS, undefined) {
+  const MODULE_NAME = 'BWAV';
+
+  const MODELS = [
+    {
+      id: 1,
+      avatar: 'images/model-1.jpg',
+      underaged: false,
+      gender: 'f',
+    },
+    {
+      id: 2,
+      avatar: 'images/model-2.jpg',
+      underaged: true,
+      gender: 'm',
+    },
+    {
+      id: 3,
+      avatar: 'images/model-3.jpg',
+      underaged: false,
+      gender: 'f',
+    }
+  ];
+
+  const COPY = {
+    agecheck: 'This is an <strong>adult-only</strong> website',
+    consent: 'By continuing to browse this website, you aggree to our <a href="#">cookie policy</a> and <a href="#">terms and conditions</a>.',
+    consentButton: 'I am older than 18',
+
+    questionIntro: 'But more important...',
+    question: 'Is #G# 18 years or older?',
+    yes: 'Yes',
+    no: 'No',
+
+    genderM: 'he',
+    genderF: 'she',
+
+    genderMFull: 'this boy',
+    genderFFull: 'this girl',
+
+    correct: 'Correct, #G# is <strong>underaged</strong>',
+    correctContent: 'But you can admit, it\'s not always easy to see if someone is of age. At the moment this picture was taken, #GF# was active as a sex worker.',
+
+    incorrect: 'Unfortunately, #G# is <strong>underaged</strong>',
+    incorrectContent: 'As you can see, it\'s not always easy to see if someone is of age. Because at the moment this picture was taken, #GF# was active as a sex worker.',
+
+    info: `<p>We do our best to avoid working with underaged sex workers, but we can never be 100% succesful. In the future, when in doubt, but still want to meet? Try asking for an ID for example. Only then will you be sure to not do anything illegal.</p>
+    <p>And if it turns out the sex worker is indeed underaged? Report this anonymously to Child Focus, by calling tollfree to <a href="tel:116 000">116 000</a> or by using <a href="#">this online form</a>. And help us in the combat against sexual exploitation of children.</p>`,
+
+    ...(BWAV_SETTINGS.content || {})
+  };
+
+  const SETTINGS = {
+    debug: false,
+
+    accentColor: 'green',
+    accentTextColor: 'white',
+    shadowColor: 'rgba(0,128,0,.25)',
+
+    logo: 'logo.png',
+
+    ageCheck: true,
+    blur: false,
+    models: MODELS,
+
+    cdnPrefix: '',
+
+    cookieAge: 30,
+    cookieName: 'bwav',
+
+    eventPrefix: 'bwav:',
+    
+    version: 'v0.1',
+
+    ...(BWAV_SETTINGS || {}),
+
+    content: COPY,
+  };
+
+  const SELECTORS = {
+    target: 'body',
+    overlay: '.bwav__overlay',
+    wrapper: '.bwav__wrapper',
+    agecheck: '.bwav__agecheck',
+  };
+
+  const CLASSES = {
+    blur: 'bwav--blur',
+    show: 'bwav--shown',
+  };
+
+  const STORE = {};
+
+  /**
+   * Select a random model from the models array
+   * @returns {object} model record
+   */
+  function selectModel() {
+    return SETTINGS.models[Math.floor(Math.random() * SETTINGS.models.length)];
+  };
+
+  function genderizeSentence(sentence, gender) {
+    const g = gender === 'f' ? SETTINGS.content.genderF : SETTINGS.content.genderM;
+    const gFull = gender === 'f' ? SETTINGS.content.genderFFull : SETTINGS.content.genderMFull;
+
+    return sentence.replace('#G#', g).replace('#GF#', gFull);
+  }
+
+  function answerQuestion(answer) {
+    if (SETTINGS.debug) { console.log(`${MODULE_NAME} answer question`, answer); }
+    triggerEvent(`${SETTINGS.eventPrefix}on_answer`, { answer, model: STORE.model });
+
+    // validate
+    const correct = STORE.model.underaged !== answer;
+
+    // setup html
+    const template = `
+      <div class="bwav__step">
+        <div class="bwav__avatar bwav__avatar--scaledown${ correct ? '' : ' bwav__avatar--blur' }">
+          <div class="bwav__avatar__image" style="background-image: url(${STORE.model.avatar});"></div>
+        </div>
+
+        <div class="bwav__content">
+          ${ correct ? `
+          <p class="bwav__title">${ genderizeSentence(SETTINGS.content.correct, STORE.model.gender) }</p>
+          <p class="bwav__intro">${ genderizeSentence(SETTINGS.content.correctContent, STORE.model.gender) }</p>
+        ` : `
+            <p class="bwav__title">${ genderizeSentence(SETTINGS.content.incorrect, STORE.model.gender) }</p>
+            <p class="bwav__intro">${ genderizeSentence(SETTINGS.content.incorrectContent, STORE.model.gender) }</p>
+          ` }
+          ${ SETTINGS.content.info }
+        </div>
+
+        <div class="bwav__footer">
+
+        </div>
+      </div>
+    `;
+
+    STORE.wrapper.innerHTML = template;
+  }
+
+  /**
+   * Public function to start the survey
+   */
+  function start() {
+    if (SETTINGS.debug) { console.log(`${MODULE_NAME} start`, STORE.model); }
+    triggerEvent(`${SETTINGS.eventPrefix}on_start`, { model: STORE.model });
+
+    // setup html
+    const template = `
+    <div class="bwav__step">
+      <div class="bwav__avatar bwav__avatar--scaleup">
+        <div class="bwav__avatar__image" style="background-image: url(${ STORE.model.avatar });"></div>
+      </div>
+
+      <div class="bwav__content">
+        <p>${SETTINGS.content.questionIntro}</p>
+        <p class="bwav__question">${SETTINGS.content.question}</p>
+
+        <div class="bwav__actions">
+          <button class="bwav__button" onclick={BWAV.answer(true)}><span>${SETTINGS.content.yes}</span></button>
+          <button class="bwav__button" onclick={BWAV.answer(false)}><span>${SETTINGS.content.no}</span></button>
+        </div>
+      </div>
+    </div>
+    `;
+
+    STORE.wrapper.innerHTML = template;
+
+    STORE.target.classList.add(CLASSES.show);
+  }
+
+  function ageCheck() {
+    const template = `
+    <div class="bwav__agecheck">
+      <div class="bwav__avatar">
+        <div class="bwav__avatar__image" style="background-image: url(${SETTINGS.logo});"></div>
+      </div>
+
+      <div class="bwav__content">
+        <p>${SETTINGS.content.agecheck}</p>
+        <div class="bwav__agecheck__consent">${SETTINGS.content.consent}</div>
+
+        <button class="bwav__button" onclick="BWAV.start()"><span>${SETTINGS.content.consentButton}</span></button>
+      </div>
+
+      <div class="bwav__footer">
+        <a href="#">Meer informatie</a>
+      </div>
+    </div>
+    `;
+
+    STORE.wrapper.innerHTML = template;
+  }
+
+  /**
+   * Prepare HTML overlay and wrapper
+   */
+  function setupHTML() {
+    if (SETTINGS.debug) { console.log(`${MODULE_NAME} setup HTML`); }
+    const target = document.querySelector(SELECTORS.target);
+
+    try {
+      const scaffold = `
+        <div class="bwav__overlay">
+          <div class="bwav__wrapper"></div>
+        </div>
+      `;
+      
+      const temp = document.createElement('div');
+      temp.innerHTML = scaffold;
+
+      
+      STORE.target = target;
+      STORE.overlay = temp.querySelector(SELECTORS.overlay);
+      STORE.wrapper = temp.querySelector(SELECTORS.wrapper);
+
+      target.appendChild(STORE.overlay);
+
+      if (SETTINGS.debug) { console.log(`${MODULE_NAME} HTML setup done`); }
+    } catch(e) {
+      if (SETTINGS.debug) {
+        console.warn(`${MODULE_NAME} HTML setup failed`);
+        console.error(e);
+      }
+    }
+    
+  }
+
+  /**
+   * Trigger an event to notify the website of certain actions
+   * @param {string} eventName 
+   * @param {object} payload 
+   */
+  function triggerEvent(eventName = '', payload = {}) {
+    if (SETTINGS.debug) { console.log(`${MODULE_NAME} trigger event:`, eventName, payload); }
+    
+    try {
+      if (!eventName) {
+        throw new Error('triggerEvent needs an eventName');
+      }
+
+      const event = new CustomEvent(eventName, payload);
+      window.dispatchEvent(event);
+      if (SETTINGS.debug) { console.log(`${MODULE_NAME} event triggered:`, eventName, payload); }
+    } catch(e) {
+      if (SETTINGS.debug) {
+        console.warn(`${MODULE_NAME} - trigger event failed:`, eventName, payload);
+        console.error(e);
+      }
+    }
+  }
+
+  /**
+   * Add classes to necessary items
+   */
+  function addClasses() {
+    // if (SETTINGS.blur) {
+    //   if (SETTINGS.debug) { console.log(`${MODULE_NAME} blur content`); }
+    //   document.body.classList.add('bwav--blur');
+    // }
+
+    if (SETTINGS.ageCheck) {
+      STORE.target.classList.add(CLASSES.show);
+    }
+  }
+
+  /**
+   * Inject stylesheet and custom CSS properties
+   */
+  function addStyles() {
+    if (SETTINGS.debug) { console.log(`${MODULE_NAME} adding styles`); }
+
+    try {
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = `${SETTINGS.cdnPrefix}/bewatched-age-verification.${SETTINGS.version}.css`;
+
+      document.head.appendChild(link);
+
+      if (SETTINGS.debug) { console.log(`${MODULE_NAME} stylesheet added`); }
+
+      document.documentElement.style.setProperty('--bwav-color--primary', SETTINGS.accentColor);
+      document.documentElement.style.setProperty('--bwav-color--primary-text', SETTINGS.accentTextColor);
+      document.documentElement.style.setProperty('--bwav-color--primary-shadow', SETTINGS.shadowColor);
+
+      if (SETTINGS.debug) { console.log(`${MODULE_NAME} custom css properties added to :root`); }
+
+    } catch(e) {
+      if (SETTINGS.debug) {
+        console.warn(`${MODULE_NAME} - addStyles failed`);
+        console.error(e);
+      }
+    }
+  }
+
+  /**
+   * Initialize plugin, prepare DOM and inject necessary resources
+   */
+  function init() {
+    if (SETTINGS.debug) { console.log(`${MODULE_NAME} init`); }
+    triggerEvent(`${SETTINGS.eventPrefix}on_init`, SETTINGS);
+  
+    addStyles();
+    setupHTML();
+    addClasses();
+
+    if (SETTINGS.ageCheck) {
+      ageCheck();
+    }
+
+    // select model
+    STORE.model = selectModel();
+
+    if (SETTINGS.debug) { console.log(`${MODULE_NAME} initialisation finished`); }
+    triggerEvent(`${SETTINGS.eventPrefix}after_init`);
+  };
+
+  document.addEventListener('DOMContentLoaded', init);
+
+  return {
+    start,
+    answer: answerQuestion,
+  };
+}(window, window.BWAV_SETTINGS || {}));
