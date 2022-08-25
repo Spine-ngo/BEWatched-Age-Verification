c57489881ff00d441462bbe86ecc2cd35b4ced1e
Thijs van der Heijden <thijsvdh12@gmail.com>
2022-08-26 01:22:24 +0200

    Add defaults

---
+++ src/defaults/options.default.ts
@@ -0,0 +1,22 @@
+import { Options } from "../types/options.type";
+import { modelsDefault } from "./models.default";
+import { translationsDefault } from "./translations.default";
+
+export const optionsDefault: Options = {
+  autoStart: true,
+  debug: false,
+  showCloseButton: false,
+  accentColor: '#F26457',
+  accentTextColor: '#fff',
+  shadowColor: 'rgba(242,100,87,.25)',
+  blur: true,
+  models: modelsDefault,
+  cookieAge: 30,
+  cookieName: 'age-verification',
+  showMax: 5,
+  eventPrefix: 'av',
+  language: 'en',
+  translations: translationsDefault,
+  brands: [],
+  showDefaultBrand: true
+}
---
+++ src/defaults/translations.default.ts
@@ -0,0 +1,10 @@
+import { Translations } from "../types/translations.type";
+
+export const translationsDefault: Translations = {
+  "en": {
+    "hello-world": "Hello World!",
+  },
+  "nl": {
+    "hello-world": "Hallo Wereld!",
+  }
+}
