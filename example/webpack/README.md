55efc814dc617f59303802766bd4a336634d538f
Thijs van der Heijden <thijsvdh12@gmail.com>
2022-08-25 13:59:32 +0200

    Add examples

---
+++ example/webpack/README.md
@@ -0,0 +1,7 @@
+# Webpack example
+
+To run this example run the following command in the root of the project:
+
+```bash
+npm run example:webpack
+```
---
+++ example/webpack/webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    })
+  ],
+  mode: 'development',
+  output: {
+    clean: true,
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+};
