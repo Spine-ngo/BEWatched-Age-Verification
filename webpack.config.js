4ad7ff8540841064e908dc93e41f49d97129a0a2
Thijs van der Heijden <thijsvdh12@gmail.com>
2022-09-05 15:51:36 +0200

    Fix tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ const config = {
       cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, './dist')],
     }),
   ],
-  devtool: 'eval-source-map',
+  devtool: isProduction ? 'source-map': 'eval-source-map',
   module: {
     rules: [
       {
